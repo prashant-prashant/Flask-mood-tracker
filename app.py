b905bce433576cba03b13714973c01c8c8a27011
Prashant Gupta <prashantgupta7616@gmail.com>
2026-02-20 10:26:55 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
 from flask_cors import CORS
 import csv
 import os
@@ -266,5 +266,8 @@ def get_insights():
     insights = tracker.get_insights()
     return jsonify(insights)
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
